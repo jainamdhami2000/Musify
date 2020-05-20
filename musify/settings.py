9e15dc975c716bee5d1dbd6e59a1ef5589660421
Jainam <jainamdhami2000@gmail.com>
2020-05-21 03:44:27 +0530

    Updated Settings and req

--- musify/settings.py
+++ musify/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,3 +157,5 @@ SOCIALACCOUNT_PROVIDERS = {
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = 'login'
+
+django_heroku.setting(locals())
