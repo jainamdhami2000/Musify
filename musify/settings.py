b46b4eb6c0051e0848dd6ff537247540b54d8c1b
Jainam <jainamdhami2000@gmail.com>
2020-05-21 02:28:41 +0530

    Updated static_root

--- musify/settings.py
+++ musify/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
